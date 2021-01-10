1c5d79885ec032cac0fa66c7ba57bfecb79d0546
Glenn Marcus <glenn@cliqconsulting.com>
2021-01-10 00:21:25 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          I was recently changed!
+          I was changed on the feature branch!
         </p>
         <a
           className="App-link"
